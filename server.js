1406e93e388822ba0fbef5316c0ef0ad908ef3de
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-11-01 13:01:06 -0500

    Add CORS

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ const Patient = require('./models/Patient');
 
 const app = express();
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
 // Body parser
 app.use(express.json());
 
