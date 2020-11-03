178d6a82af49d649698cd66675cdae73a68222ed
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-11-03 18:32:12 -0500

    Change model

--- models/Record.js
+++ models/Record.js
@@ -3,29 +3,29 @@ const mongoose = require('mongoose');
 const RecordSchema = new mongoose.Schema({
     bodyTemperature: {
         type: Number,
-        required: [true, 'Please add Body Temperature']
+        required: [false, 'Please add Body Temperature']
     },
     pulseRate:{
         type: Number,
-        required: [true, 'Please add pulse rate']
+        required: [false, 'Please add pulse rate']
     },
 
     respirationRate: {
         type: Number,
-        required: [true, 'Please add respiration rate']
+        required: [false, 'Please add respiration rate']
     },
     systolicBP:{
         type: Number,
-        required: [true, 'Please add systolic bp']
+        required: [false, 'Please add systolic bp']
     },
     diastolicBP:{
         type: Number,
-        required: [true, 'Please add diastolic bp']
+        required: [false, 'Please add diastolic bp']
     },
 
     o2Sat: {
         type: Number,
-        required: [true, 'Please add oxygen saturation']
+        required: [false, 'Please add oxygen saturation']
     },
     createdAt: {
         type: Date,
